e29116910953811193cbdf5c849885060ee9e1cc
Klara Kuret <kuretklara@gmail.com>
2023-04-18 16:20:19 +0200

    add environments

--- analysis/Preprocessing1_AnnotateGencodeUtrsAs3or5prime/AnnotateUtsInFilteredGencodeAnnot.sh
+++ analysis/Preprocessing1_AnnotateGencodeUtrsAs3or5prime/AnnotateUtsInFilteredGencodeAnnot.sh
@@ -2,7 +2,6 @@
 
 # Running in JupyterAnalysis conda environment, using pyranges (https://github.com/biocore-NTNU/pyranges) and https://github.com/MuhammedHasan/gencode_utr_fix
 
-# Full annotation
 gencode_utr_fix \
 --input_gtf '../../data/genomes/Goodwright_m39/gencode.vM28.primary_assembly.annotation.gtf.gz' \
 --output_gtf '../../data/genomes/Goodwright_m39/gencode.vM28.primary_assembly.annotation.gencode_utr_fix.gtf.gz'
